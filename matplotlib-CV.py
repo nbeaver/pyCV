3aaf60691115bf3aae7a817ba1dee6513f595366
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-04-16 14:25:36 -0500

    Refactoring and reorganizing a bit.

--- matplotlib-CV.py
+++ matplotlib-CV.py
@@ -2,8 +2,10 @@
 import matplotlib.pyplot
 import csv
 
-def detect_reversals(list):
-    reversal_indices = []
+def get_extrema(list):
+    extrema = []
+    maxima = []
+    minima = []
     for i, val in enumerate(list):
         if i > 2:
             delta =          list[i - 0] - list[i - 1]
@@ -12,14 +14,14 @@ def detect_reversals(list):
             # If delta and delta_previous have different signs, we've detected a beginning or end of a cycle
             # TODO: implement debouncing
             if delta*delta_previous < 0:
-                reversal_indices.append(i)
-                #print "Sign reversal in voltage detected at line", i
-                #print "V = ", val
-                #print "dV =", delta
-                #print "dV_previous =", delta_previous
+                extrema.append(i)
+                if cmp(delta, delta_previous) == -1:
+                    maxima.append(i)
+                else:
+                    minima.append(i)
         else:
             pass
-    return reversal_indices
+    return extrema, minima, maxima
 
 voltage_list = []
 current_list = []
@@ -40,18 +42,22 @@ with open("V-I.csv") as csvfile:
         assert n == len(voltage_list)
         assert n == len(current_list)
 
-detect_reversals(voltage_list)
-
-# Make room for larger text
-from matplotlib import rcParams
-rcParams.update({'figure.autolayout': True})
-
 A_to_mA = 1000 # 1000 milliamps per amp
+current_list_mA = [A_to_mA*current for current in current_list]
 
-matplotlib.pyplot.plot(voltage_list, [A_to_mA*current for current in current_list])
-matplotlib.pyplot.xlabel('Cell potential versus Li [V]', fontsize=20)
-matplotlib.pyplot.ylabel('Cell current [mA]', fontsize=20)
-matplotlib.pyplot.title('Mesoporous carbon', fontsize=30)
-matplotlib.pyplot.savefig('all-cycles.png', bbox_inches='tight')
-matplotlib.pyplot.savefig('all-cycles.jpg', bbox_inches='tight')
-matplotlib.pyplot.show()
+_, _, voltage_maximas  = get_extrema(voltage_list)
+
+matplotlib.pyplot.plot(voltage_list, current_list_mA)
+
+def saveplot(filename):
+    # Make room for larger text
+    from matplotlib import rcParams
+    rcParams.update({'figure.autolayout': True})
+
+    matplotlib.pyplot.xlabel('Cell potential versus Li [V]', fontsize=20)
+    matplotlib.pyplot.ylabel('Cell current [mA]', fontsize=20)
+    matplotlib.pyplot.title('Mesoporous carbon', fontsize=24)
+    matplotlib.pyplot.savefig(filename+'.png', bbox_inches='tight')
+    matplotlib.pyplot.savefig(filename+'.jpg', bbox_inches='tight')
+
+saveplot('all-cycles')
