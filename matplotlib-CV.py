e6206d0fbfb00a5b2dfbbbbd9b33dcb4a06b1a38
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-04-16 11:51:04 -0500

    Added code to detect when the voltage changes sign.

--- matplotlib-CV.py
+++ matplotlib-CV.py
@@ -2,24 +2,39 @@
 import matplotlib.pyplot
 import csv
 
-a = [1.0, 2.0, 3.0]
-
-b = [4.0, 6.0, 7.0]
-
-#matplotlib.pyplot.plot(a, b)
-#matplotlib.pyplot.show()
-
-voltage = []
-current = []
+voltage_list = []
+current_list = []
 with open("V-I.csv") as csvfile:
     rows = csv.reader(csvfile, delimiter='\t')
     for row in rows:
-        if rows.line_num == 1: 
-            print row
-            continue # don't include the headers in the first row
-        # We don't need to convert them to reals, since pyplot does this already
-        voltage.append(row[0])
-        current.append(row[1])
+        number_of_rows_to_skip = 1
+        if rows.line_num <= number_of_rows_to_skip: 
+            continue # skip headers
 
-matplotlib.pyplot.plot(voltage, current)
+        i = rows.line_num - number_of_rows_to_skip # datapoint index to be added
+        n = i - 1 # nominal number of datapoints
+        assert n == len(voltage_list)
+        assert n == len(current_list)
+
+        voltage = float(row[0])
+        current = float(row[1])
+
+        if n > 2:
+            voltage_previous = voltage_list[-1]
+
+            dV = voltage_previous  - voltage
+            dV_previous = voltage_list[-2] - voltage_previous 
+
+            # If dV and dV_previous have different signs, we've detected a beginning or end of a cycle
+            # TODO: implement debouncing
+            if dV*dV_previous < 0:
+                print "Sign reversal in voltage detected at line", rows.line_num
+                print "dV =", dV
+                print "dV_previous =", dV_previous
+
+        voltage_list.append(voltage)
+        current_list.append(current)
+
+
+matplotlib.pyplot.plot(voltage_list, current_list)
 matplotlib.pyplot.show()
