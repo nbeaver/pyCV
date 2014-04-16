62e55ec877710b458a461853c4836045405762e4
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-04-16 12:13:49 -0500

    Moved reversal detection into a separate function.

--- matplotlib-CV.py
+++ matplotlib-CV.py
@@ -2,6 +2,25 @@
 import matplotlib.pyplot
 import csv
 
+def detect_reversals(list):
+    reversal_indices = []
+    for i, val in enumerate(list):
+        if i > 2:
+            delta =          list[i - 0] - list[i - 1]
+            delta_previous = list[i - 1] - list[i - 2]
+
+            # If delta and delta_previous have different signs, we've detected a beginning or end of a cycle
+            # TODO: implement debouncing
+            if delta*delta_previous < 0:
+                reversal_indices.append(i)
+                print "Sign reversal in voltage detected at line", i
+                print "V = ", val
+                print "dV =", delta
+                print "dV_previous =", delta_previous
+        else:
+            pass
+    return reversal_indices
+
 voltage_list = []
 current_list = []
 with open("V-I.csv") as csvfile:
@@ -11,30 +30,17 @@ with open("V-I.csv") as csvfile:
         if rows.line_num <= number_of_rows_to_skip: 
             continue # skip headers
 
-        i = rows.line_num - number_of_rows_to_skip # datapoint index to be added
-        n = i - 1 # nominal number of datapoints
-        assert n == len(voltage_list)
-        assert n == len(current_list)
-
         voltage = float(row[0])
         current = float(row[1])
-
-        if n > 2:
-            voltage_previous = voltage_list[-1]
-
-            dV = voltage_previous  - voltage
-            dV_previous = voltage_list[-2] - voltage_previous 
-
-            # If dV and dV_previous have different signs, we've detected a beginning or end of a cycle
-            # TODO: implement debouncing
-            if dV*dV_previous < 0:
-                print "Sign reversal in voltage detected at line", rows.line_num
-                print "dV =", dV
-                print "dV_previous =", dV_previous
 
         voltage_list.append(voltage)
         current_list.append(current)
 
+        n = rows.line_num - number_of_rows_to_skip # nominal number of datapoints
+        assert n == len(voltage_list)
+        assert n == len(current_list)
+
+detect_reversals(voltage_list)
 
 matplotlib.pyplot.plot(voltage_list, current_list)
 matplotlib.pyplot.show()
